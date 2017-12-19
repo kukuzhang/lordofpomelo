d187fc0fa174ac240ca5c5ce0f6a3495fbd6505c
joe <kuku349i@qq.com>
2017-12-19 10:24:51 +0800

    add file

---
+++ web-server/lib/dao/mysql/dao-pool.js
@@ -0,0 +1,60 @@
+var _poolModule = require('generic-pool');
+var mysqlConfig = require('../../../../shared/config/mysql');
+var mysql = require('mysql');
+
+var env = process.env.NODE_ENV || 'development';
+if(mysqlConfig[env]) {
+  mysqlConfig = mysqlConfig[env];
+}
+
+/*
+ * Create mysql connection pool.
+ */
+// var createMysqlPool = function(){
+//   return _poolModule.createPool({
+//     name     : 'mysql',
+//     create   : function(callback) {
+//       var client = mysql.createConnection({
+//         host: mysqlConfig.host,
+//         user: mysqlConfig.user,
+//         password: mysqlConfig.password,
+//         database: mysqlConfig.database
+//       });
+//       callback(null, client);
+//     },
+//     destroy  : function(client) { client.end(); },
+//     max      : 10,
+//     idleTimeoutMillis : 30000,
+//     log : false
+//   });
+// };
+
+
+var createMysqlPool = function(){
+  return _poolModule.createPool({
+    name     : 'mysql',
+    create: function(){
+      return new Promise(function(resolve, reject){
+        var client = mysql.createConnection({
+          host: mysqlConfig.host,
+          user: mysqlConfig.user,
+          password: mysqlConfig.password,
+          database: mysqlConfig.database
+        });
+        resolve(client);
+      })
+    },
+
+    destroy  : function(client) {
+      return new Promise(function(resolve){
+        client.end();
+      })
+    }
+  }, {
+    max      : 10,
+    idleTimeoutMillis : 30000,
+    log : false
+  });
+};
+
+exports.createMysqlPool = createMysqlPool;
